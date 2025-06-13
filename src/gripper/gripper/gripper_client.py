fcf936a87a839dc2d2ed048bd779383ec1251e56
David Nie <davidnie0418@gmail.com>
2025-06-13 11:55:21 +1000

    add reset gripper functionality in gripper package, remove velocity and acceleration setting in moveit as they temper with ur settings

--- src/gripper/gripper/gripper_client.py
+++ src/gripper/gripper/gripper_client.py
@@ -2,14 +2,19 @@
 
 import rclpy
 from rclpy.node import Node
-from custom_interface.srv import GripperCmd
+from custom_interface.srv import GripperCmd, ResetGripperCmd
 
 class GripperClient(Node):
     def __init__(self):
         super().__init__('gripper_client')
         self.cli = self.create_client(GripperCmd, 'gripper_cmd')
         while not self.cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again...')
+            self.get_logger().info('gripper service not available, waiting again...')
+
+        self.cli_2 = self.create_client(ResetGripperCmd, 'reset_gripper_cmd')
+        while not self.cli_2.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('reset gripper service not available, waiting again...')
+
         self.get_logger().info('Connected to gripper server')
         
     def send_request(self, width, force):
@@ -17,6 +22,13 @@ class GripperClient(Node):
         req.width = width
         req.force = force
         self.future = self.cli.call_async(req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+    
+    def reset_gripper(self):
+        req = ResetGripperCmd.Request()
+        req.reset_gripper = True
+        self.future = self.cli_2.call_async(req)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
 
@@ -28,17 +40,21 @@ def main(args=None):
     try:
         while True:
             # Get user input
-            user_input = input("Enter width and force (default 40N) separated by space: ").strip()
-            
+            user_input = input("Enter width and force (default 40N) separated by space, or 'reset' for resetting the gripper: ").strip()
+            response = ''
+
             if not user_input:
                 continue
                 
-            parts = user_input.split()
-            width = int(parts[0])
-            force = 40 if len(parts) < 2 else int(parts[1])
-            
-            # Send request
-            response = client.send_request(width, force)
+            if user_input.strip() == "reset":
+                response = client.reset_gripper()
+            else:
+                parts = user_input.split()
+                width = int(parts[0])
+                force = 40 if len(parts) < 2 else int(parts[1])
+                
+                # Send request
+                response = client.send_request(width, force)
             
             # Display result
             if response.success:
